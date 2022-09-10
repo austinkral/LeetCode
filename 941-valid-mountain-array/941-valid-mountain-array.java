fe1527f549d1948c0fe62c9621907cc285abed49
Austin Kral <71894727+austinkral@users.noreply.github.com>
2022-09-10 15:30:50 -0400

    Time: 3 ms (34.70%), Space: 53.9 MB (68.30%) - LeetHub

---
+++ 941-valid-mountain-array/941-valid-mountain-array.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        int l = 0, r = arr.length - 1;
+        while (l + 1 < arr.length - 1 && arr[l] < arr[l + 1]) l++;
+        while (r - 1 > 0 && arr[r] < arr[r - 1]) r--;
+        return l == r && r != arr.length - 1;
+    } // validMountainArray
+} // Solution
