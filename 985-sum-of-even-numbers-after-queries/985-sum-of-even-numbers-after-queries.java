af2f5b6bebb6f8ef9d3910f8b75bcd13ded3b246
Austin Kral <71894727+austinkral@users.noreply.github.com>
2022-09-20 23:43:11 -0400

    Time: 21 ms (5.47%), Space: 69 MB (70.74%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
         int[] answer = new int[queries.length];
@@ -7,6 +8,25 @@ class Solution {
             esum = nums[q[1]] % 2 == 0 ? esum -= nums[q[1]] : esum;
             nums[q[1]] += q[0];
             esum = nums[q[1]] % 2 == 0 ? esum += nums[q[1]] : esum;
+            answer[i++] = esum;
+        } // for
+        return answer;
+    } // sumEvenAfterQueries
+} // Solution
+*/
+
+
+class Solution {
+    public int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
+        int[] answer = new int[queries.length];
+        int esum = 0, i = 0;
+        for (int n : nums) if (n % 2 == 0) esum += n;
+        for (int[] q : queries) {
+            int original = nums[q[1]];
+            nums[q[1]] += q[0];
+            if (original % 2 == 0 && q[0] % 2 == 0) esum += q[0];
+            if (original % 2 == 0 && q[0] % 2 != 0) esum -= original;
+            if (original % 2 != 0 && q[0] % 2 != 0) esum += nums[q[1]];
             answer[i++] = esum;
         } // for
         return answer;
