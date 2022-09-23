5a6d63eb308a4dc927ba7cfdd68364e5690cdff9
Austin Kral <71894727+austinkral@users.noreply.github.com>
2022-09-23 02:18:30 -0400

    Time: 41 ms (7.76%), Space: 46.7 MB (11.37%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,7 +1,5 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        //if (s == null && t == null) return true;
-        //if (s == null || t == null) return false;
         if (s.length() != t.length()) return false;
         HashMap<Character, Integer> freq = new HashMap<Character, Integer>();
         for (char c : s.toCharArray()) {
