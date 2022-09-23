cb71e16359fb16ff9bf92fbd1b1a2da035e5ca47
Austin Kral <71894727+austinkral@users.noreply.github.com>
2022-09-23 02:17:52 -0400

    Time: 27 ms (17.04%), Space: 46.9 MB (9.41%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s == null && t == null) return true;
+        if (s == null || t == null) return false;
+        if (s.length() != t.length()) return false;
+        HashMap<Character, Integer> freq = new HashMap<Character, Integer>();
+        for (char c : s.toCharArray()) {
+            if (freq.containsKey(c)) freq.put(c, freq.get(c) + 1);
+            else freq.put(c, 1);
+        } // for
+        for (char c : t.toCharArray()) {
+            if (freq.getOrDefault(c, 0) == 0) return false;
+            else freq.put(c, freq.get(c) - 1);
+        } // for
+        return true;
+    } // isAnagram
+} // Solution
