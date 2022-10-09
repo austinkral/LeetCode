8ffdb8f0a7f4739cc1c9080909dd9ddcb47dd5b4
Austin Kral <71894727+austinkral@users.noreply.github.com>
2022-10-09 17:25:59 -0400

    Time: 34 ms (24.04%), Space: 55.3 MB (5.03%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        List<List<Integer>> freqCount = new ArrayList<List<Integer>>(nums.length + 1);
+        System.out.println(freqCount.size());
+        Map<Integer, Integer> freq = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length + 1; i++) {
+            freqCount.add(new ArrayList<Integer>());
+        }
+        int[] sol = new int[k];
+        int solCount = 0;
+        for (int num : nums) freq.put(num, freq.getOrDefault(num, 0) + 1);
+        for (int key : freq.keySet()) {
+            if (freqCount.get(freq.get(key)) == null) {
+                freqCount.set(freq.get(key), new ArrayList<Integer>());
+            } // if
+            freqCount.get(freq.get(key)).add(key);
+        } // for
+        for (int i = freqCount.size() - 1; i >= 0 && solCount < k; i--) {
+                for (int j : freqCount.get(i)) {
+                 sol[solCount++] = j;
+                } // for
+        } // for
+        return sol;
+    }
+}
