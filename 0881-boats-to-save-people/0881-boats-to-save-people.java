5a399b491dc41cd3c9600def00c4bb7535e23786
Austin Kral <71894727+austinkral@users.noreply.github.com>
2022-11-19 21:20:50 -0500

    Time: 33 ms (19.68%), Space: 65.6 MB (29.65%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int output=0;
+        Arrays.sort(people);
+        int max = people.length - 1;
+        int min = 0;
+        for (int i = 0; i < people.length; i++) {
+            if (max < min) {
+                return output;
+            } //if
+            if (!(people[max] + people[min] > limit)) {
+                min++;
+            } //if
+            output++;
+            max--;
+        } //for
+        return output;
+    } //numRescueBoats
+} // Solution
