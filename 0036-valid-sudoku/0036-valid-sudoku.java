9a1626e94b0adba82c58b5d26bc52aa390c3fdd9
Austin Kral <71894727+austinkral@users.noreply.github.com>
2022-11-21 21:43:33 -0500

    Time: 38 ms (7.86%), Space: 48.6 MB (14.53%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         HashSet <String> seen = new HashSet<>();
@@ -17,3 +18,38 @@ class Solution {
         return true;
     } //isValidSudoku
 } //Solution
+*/
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> rows = new HashSet<>();
+        HashSet<String> cols = new HashSet<>();
+        HashSet<String> boxes = new HashSet<>();
+        
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[i][j] == '.') continue;
+                if (rows.contains("R" + i + "," + board[i][j])) return false;
+                rows.add("R" + i + "," + board[i][j]);
+            } // for
+        } // for
+        
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[i][j] == '.') continue;
+                if (cols.contains("C" + j + "," + board[i][j])) return false;
+                cols.add("C" + j + "," + board[i][j]);
+            } // for
+        } // for
+        
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[i][j] == '.') continue;
+                if (boxes.contains("R" + i/3 + "C" + j/3 + "," + board[i][j])) return false;
+                boxes.add("R" + i/3 + "C" + j/3 + "," + board[i][j]);
+            } // for
+        } // for
+        
+        return true;
+    } // isValidSudoku
+} // Solution
